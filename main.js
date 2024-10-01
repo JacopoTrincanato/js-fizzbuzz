b9ce200efb54e4a2f5fe711c557f79ed21e393a3
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-01 15:39:38 +0200

    try to add a condition that print FizzBuzz instead of number three and number five multiples

--- main.js
+++ main.js
@@ -19,11 +19,15 @@ for (let i = 1; i <= 100; i++) {
 //imposto la condizione per il quale stampi 'Fizz' al posto dei multipli di 3
     if (i % 3 == 0) {
         console.log('Fizz');
+
 //imposto la condizione per il quale stampi 'Buzz' al posto dei multipli di 5   
     }else if (i % 5 == 0) {
         console.log('Buzz');
+//imposto la condizione per il quale stampi 'FizzBuzz' al posto dei multipli sia di 3 che di 5        
+    }else if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
         
-      }else {
+    }else {
         console.log(i);
         
     }
