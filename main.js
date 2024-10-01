29d0c172d97f5a179e7fdd6f38fedb9c759eb2a4
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-01 15:16:36 +0200

    creo variabile i

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 console.log('hello');
 
 //FASE 1: preparazione
-//creo il loop che mi stampi i numeri da 1 a 100 
- 
-for (let i = 1; i <= 100; i++) {
-   
-    console.log(i);
-    
-  }
+//creo la variabile i
+let i
 
 //FASE 2: raccolta Dati
 //assegno un valore a 
@@ -26,7 +21,13 @@ for (let i = 1; i <= 100; i++) {
 
 
 //FASE 3: elaborazione dati
-
+//creo il loop che mi stampi i numeri da 1 a 100 
+ 
+for (let i = 1; i <= 100; i++) {
+   
+    console.log(i);
+    
+  }
 
 //FASE 4: output
 //stampo in console
