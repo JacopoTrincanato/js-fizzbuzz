fd6c796f68575e56c8a8e9c5dd06aeff0c2b200f
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-01 14:24:55 +0200

    paste basic js structure

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 console.log('hello');
+
+//FASE 1: preparazione
+//creo variabile dove salvare 
+
+
+//creo variabile dove salvare 
+
+
+//creo variabile dove salvare 
+
+
+//creo variabile dove salvare 
+
+
+//creo variabile dove salvare 
+
+
+//creo variabile dove salvare 
+
+
+//FASE 2: raccolta Dati
+//assegno un valore a 
+
+
+//assegno un valore a 
+
+
+//assegno un valore a 
+
+
+//assegno un valore a 
+
+
+//assegno un valore a 
+
+
+//FASE 3: elaborazione dati
+
+
+//FASE 4: output
+//stampo in console
+
